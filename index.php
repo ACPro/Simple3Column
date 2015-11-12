cda18b87b5d6a83164935c3d0f7be12e12828a84
rongoffacpro <ron.goff@acpro.com>
2015-11-12 08:52:52 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <html>
 	
 <head>
-    <title>3 Test</title>
-    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    	<title>3 Test</title>
+    	<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 </head>
 
