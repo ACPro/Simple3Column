ba5e4d15c515f1601f16639ac57e9f4428043d68
rongoffacpro <ron.goff@acpro.com>
2015-11-12 08:52:14 -0800

    Create SS3.php

---
+++ SS3.php
@@ -0,0 +1,6 @@
+<span class="Side_Link2" id="id-1" style="cursor: pointer">1</span><br>
+<span class="Side_Link2" id="id-2" style="cursor: pointer">2</span><br>
+<span class="Side_Link2" id="id-3" style="cursor: pointer">3</span><br>
+<span class="Side_Link2" id="id-4" style="cursor: pointer">4</span><br>
+<span class="Side_Link2" id="id-5" style="cursor: pointer">5</span><br>
+<span class="Side_Link2" id="id-6" style="cursor: pointer">6</span><br>
